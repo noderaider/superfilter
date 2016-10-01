93d3ba8d3f01f4367717866e0760aa9d235fb014
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-09-30 17:22:41 -0700

    fixes and more unit tests

--- src/lib/__tests__/superfilter.test.js
+++ src/lib/__tests__/superfilter.test.js
@@ -11,10 +11,55 @@ describe('superfilter', () => {
     })
   }
 
+
   jsonTest('filters on key', { foo: 'bar' }, 'foo', {}, { foo: 'bar'})
   jsonTest('filters on value', { foo: 'bar' }, 'bar', {}, { foo: 'bar'})
   jsonTest('filters only correct key', { foo: 'bar', fu: 'bra' }, 'foo', {}, { foo: 'bar'})
   jsonTest('filters only correct value', { foo: 'bar', fu: 'bra' }, 'bra', {}, { fu: 'bra'})
+
+  jsonTest( 'matchKeys false does not take keys into account'
+          , { fu: 'bar', bar: 'fu' }
+          , 'bar'
+          , { matchKeys: false }
+          , { fu: 'bar' }
+          )
+
+  jsonTest( 'walks only deep keys when specified'
+          , { foo: { not: 'bar' }, fu: { yep: 'bar' } }
+          , 'bar'
+          , { walkKeys: [ 'fu' ] }
+          , { foo: { not: 'bar' }, fu: { yep: 'bar' } }
+          )
+
+  jsonTest( 'filters only on non-deep keys when specified'
+          , { foo: 'nada', fu: { yep: 'bar' } }
+          , 'nada'
+          , { walkKeys: [ 'fu' ] }
+          , { foo: 'nada' }
+          )
+
+  jsonTest( 'bypasses filtering of non filterKeys when specified'
+          , { foo: 'nada', fu: { yep: 'bar' }, bra: 'foo' }
+          , 'bar'
+          , { filterKeys: [ 'fu', 'bra' ] }
+          , { foo: 'nada', fu: { yep: 'bar' } }
+          )
+
+  jsonTest( 'walks nested keys and filters on specific key'
+          , [ { title: 'foo', children: [ { title: 'bar' } ] }
+            , { title: 'bar', children: [ { title: 'foo' } ] }
+            ]
+          , 'bar'
+          , { matchKeys: false, walkKeys: [ 'children' ], filterKeys: [ 'title' ] }
+          , [ { title: 'foo', children: [ { title: 'bar' } ] }
+            , { title: 'bar' }
+            ]
+          )
+
+
+  jsonTest('does not match keys non-matching keys', { foo: 'bar', fu: 'bra' }, 'bra', {}, { fu: 'bra'})
+
+
 
   jsonTest('highlights', { foo: 'bar', fu: 'bra' }, 'br', { insertMarks: true }, { fu: '<mark>br</mark>a'})
 })
--- src/lib/superfilter.js
+++ src/lib/superfilter.js
@@ -1,4 +1,3 @@
-
 const _emptyObject = Object.freeze({})
 const _emptyArray = Object.freeze([])
 
@@ -14,7 +13,7 @@ const resultMemo = new Map()
  * @param  {Array<Object>}  options                 Options for how to filter.
  * @return {function}                               A json object thats been filtered.
  */
-export default function superfilter (json, filter, opts = {}) {
+ function _superfilter (json, filter, opts = {}) {
   const uniqueKey = `${json}|${filter}`
   const memoized = resultMemo.get(uniqueKey)
   if(memoized) {
@@ -53,7 +52,104 @@ export default function superfilter (json, filter, opts = {}) {
 }
 
 
+
+export default function superfilter ( json
+                                    , filter
+                                    , { matchKeys = true
+                                      , insertMarks = false
+                                      , filterKeys
+                                      , walkKeys
+                                      } = {}
+                                    ) {
+
+  const opts = { matchKeys, insertMarks, filterKeys, walkKeys }
+  const uniqueKey = `${json}|${filter}|${JSON.stringify(opts)}`
+  const memoized = resultMemo.get(uniqueKey)
+  if(memoized) {
+    console.warn('--superfilter-- using memoized result!')
+    return memoized
+  }
+  if(!contextMemo.has(filter)) {
+    try {
+      contextMemo.set(filter, { filter, regex: new RegExp(filter, 'ig') })
+    } catch(err) {
+      console.warn(`--superfilter-- ${filter} is not an regexable filter, memoizing...`)
+      contextMemo.set(filter, { filter, regex: false })
+    }
+  }
+  const context = contextMemo.get(filter)
+  if(!dynamicFilterMemo.has(filter)) {
+    try {
+      const dynamicFilter = eval(filter)
+      const dynamicFilterType = typeof dynamicFilter
+      switch(dynamicFilterType) {
+        case 'function':
+          dynamicFilterMemo.set(filter, dynamicFilter)
+        default:
+          throw new Error(`Did not evaluate to a parseable type eval('${filter}') === typeof '${dynamicFilterType}'`)
+      }
+    } catch(err) {
+      console.warn(`--superfilter-- ${filter} is not an understandable dynamic filter, memoizing...`)
+      dynamicFilterMemo.set(filter, false)
+    }
+  }
+  const args = [ json, context, opts ]
+  const dynamicFilter = dynamicFilterMemo.get(filter)
+  const result = dynamicFilter ? dynamicFilter(...args) : filterData(...args)
+  resultMemo.set(uniqueKey, result)
+  return result
+}
+
+
+const isPlainObject = (value) => value !== null && value !== undefined && typeof value === 'object' && !Array.isArray(value)
+
 function createReducer (context, opts) {
+  const { filter, regex } = context
+  const { matchKeys, filterKeys, walkKeys, insertMarks } = opts
+  return function recursiveReduce (_, data) {
+    let isObjectReducer = data instanceof Array
+    let [ key, value ] = isObjectReducer ? data : [ null, data ]
+    const isPlainObjectValue = isPlainObject(value)
+    const mustWalk = walkKeys ? walkKeys.includes(key) : isPlainObjectValue
+    /*
+    console.warn(`
+--recursiveReduce--
+data: ${JSON.stringify(data)}
+key: ${key}
+value: ${JSON.stringify(value)}
+isPlainObjectValue: ${isPlainObjectValue}
+mustWalk: ${mustWalk}
+`)
+*/
+    if(filterKeys && key && !filterKeys.includes(key)) {
+      return (
+        { ..._
+        , [key]: value
+        }
+      )
+    }
+
+    const executeFilter = (target) => filterData(target, context, opts)
+    const hasKeyMatch = key ? executeFilter(key) : true
+
+
+    if(key || matchKeys || mustWalk) {
+      const filtered = executeFilter(value)
+      const _value = matchKeys && hasKeyMatch ? value : filtered
+      return isObjectReducer ? (
+        { ..._
+        , [key]: _value
+        }
+      ) : (
+        [ ..._, filtered ]
+      )
+    }
+
+    return key ? { [key]: value } : [ value ]
+  }
+}
+
+function _createReducer (context, opts) {
   const { filter, regex } = context
   const { filterKeys, walkKeys, insertMarks } = opts
   return function recursiveReduce (_, data) {
@@ -80,7 +176,7 @@ function createReducer (context, opts) {
   }
 }
 
-function filterData (json, context, opts) {
+function _filterData (json, context, opts) {
   const { filter, regex } = context
   const { filterKeys, walkKeys, insertMarks } = opts
   const jsonType = typeof json
@@ -97,6 +193,35 @@ function filterData (json, context, opts) {
       if(reducee.length === 0)
         return undefined
       return reducee.reduce(createReducer(context, opts), isArray ? _emptyArray : _emptyObject)
+    case 'undefined':
+      return undefined
+    default:
+      console.info(`--superfilter-- unmapped data type (${jsonType}) for obj ${json}`)
+      return undefined
+  }
+}
+
+function filterData (json, context, opts) {
+  const { filter, regex } = context
+  const { matchKeys, filterKeys, walkKeys, insertMarks } = opts
+  const jsonType = typeof json
+  switch(jsonType) {
+    case 'string':
+      return regex.test(json) ? (insertMarks ? json.replace(regex, '<mark>$&</mark>') : json) :  undefined
+    case 'number':
+      return json.toString().includes(filter) ? json : undefined
+    case 'object':
+      if(json === null)
+        return undefined
+      const isArray = json instanceof Array
+      const reducee = isArray ? json : Object.entries(json)
+      if(reducee.length === 0)
+        return undefined
+      return reducee.filter((x) => {
+        const [ key, value ] = isArray ? [ null, x ] : x
+        return true
+
+      }).reduce(createReducer(context, opts), isArray ? _emptyArray : _emptyObject)
     case 'undefined':
       return undefined
     default:
